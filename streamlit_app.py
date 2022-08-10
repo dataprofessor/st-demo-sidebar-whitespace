7cfdb9f47d98d78a8dc0019b87288629c457fffa
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-08-10 08:56:02 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,17 @@
+# import modules
 import streamlit as st
 
-st.title('🎈 App Name')
-
-st.write('Hello world!')
+# configure sidebar text and widgets
+st.sidebar.title("My App")
+file = st.sidebar.file_uploader("Choose a file", key='a')
+cb1 = st.sidebar.checkbox('1', key='b')
+cb2 = st.sidebar.checkbox('2', key='c')
+cb3 = st.sidebar.checkbox('3', key='d')
+cb4 = st.sidebar.checkbox('4', key='e')
+cb5 = st.sidebar.checkbox('5', key='f')
+cb7 = st.sidebar.checkbox('6', key='g')
+cb8 = st.sidebar.checkbox('7', key='h')
+cb9 = st.sidebar.checkbox('8', key='i')
+cb10 = st.sidebar.checkbox('9', key='j')
+start_btn = st.sidebar.button('Run', key='k')
+st.sidebar.text("")
